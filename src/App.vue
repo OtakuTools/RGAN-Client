9ceb18f0a6f2a48af1736c47db2f8cb5f0620950
ZTao-z <1124693098@qq.com>
2020-02-22 21:52:31 +0800

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,10 @@
 <template>
   <div id="app">
     <el-container>
-      <el-header v-if="$route.name !== 'editor' && $route.name !== 'login' && $route.name !== 'regist'" style="padding: 0; z-index: 100;">
+      <el-header v-if="$route.name !== 'editor' && $route.name !== 'login' && $route.name !== 'regist'" style="position: fixed; width: 100vw; padding: 0; z-index: 100;">
         <MenuHeader class="menu-style" />
       </el-header>
-      <el-main style="padding: 0">
+      <el-main style="margin-top: 40px; padding: 0">
         <router-view></router-view>
       </el-main>
     </el-container>
