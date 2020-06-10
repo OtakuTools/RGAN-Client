c2e7d5e8aff75ad856e5a66dd7c5974d6c24e2cc
ZTao-z <1124693098@qq.com>
2020-06-10 13:59:21 +0800

    新增博客三种状态页面

--- src/components/BlogListForCurrentUser.vue
+++ src/components/BlogListForCurrentUser.vue
@@ -37,7 +37,7 @@
                 </v-list-item-subtitle>
               </v-list-item-content>
               <v-list-item-action>
-                <v-btn icon @click="handleSelected(blog.id)">
+                <v-btn icon @click="handleSelected(blog.id)" v-if="blogStatus !== 2">
                   <v-icon color="primary lighten-1">mdi-eye-outline</v-icon>
                 </v-btn>
                 <v-btn icon @click="editBlog(blog.id)">
--- src/views/UserInfo.vue
+++ src/views/UserInfo.vue
@@ -69,9 +69,32 @@
                 </v-tabs>
               </v-tab-item>
               <v-tab-item>
-                <v-card flat>
-                  <BlogListForCurrentUser :userInfo="userInfo" :blogStatus="blogStatus.PUBLISHED" v-bind="$attrs" v-on="$listeners" />
-                </v-card>
+                <v-tabs right>
+                  <v-tab>
+                    已发布
+                  </v-tab>
+                  <v-tab>
+                    草稿箱
+                  </v-tab>
+                  <v-tab>
+                    已删除
+                  </v-tab>
+                  <v-tab-item>
+                    <v-card flat>
+                      <BlogListForCurrentUser :userInfo="userInfo" :blogStatus="blogStatus.PUBLISHED" v-bind="$attrs" v-on="$listeners" />
+                    </v-card>
+                  </v-tab-item>
+                  <v-tab-item>
+                    <v-card flat>
+                      <BlogListForCurrentUser :userInfo="userInfo" :blogStatus="blogStatus.DRAFT" v-bind="$attrs" v-on="$listeners" />
+                    </v-card>
+                  </v-tab-item>
+                  <v-tab-item>
+                    <v-card flat>
+                      <BlogListForCurrentUser :userInfo="userInfo" :blogStatus="blogStatus.DELETED" v-bind="$attrs" v-on="$listeners" />
+                    </v-card>
+                  </v-tab-item>
+                </v-tabs>
               </v-tab-item>
               <v-tab-item>
                 <v-card flat>
